61446116bf8dcf6834c4087d8d2fb34dbdea5fab
koeebeth <96339397+koeebeth@users.noreply.github.com>
2024-01-17 02:47:06 +0300

    complete task 01

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const read = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+let data = '';
+
+read.on('data', (chunk) => (data += chunk));
+read.on('end', () => console.log(data));
