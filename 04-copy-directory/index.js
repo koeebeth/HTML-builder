03a4682b18595ed4f9345a8f6d6bdd567ecc50a1
koeebeth <96339397+koeebeth@users.noreply.github.com>
2024-01-19 12:57:54 +0300

    fix deleting files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,21 +3,31 @@ const path = require('path');
 
 function copyDir() {
   const pathName = path.join(__dirname, 'files-copy/');
-  fs.mkdir(pathName, { recursive: true }, (err) => {
-    if (err) throw err;
-    fs.readdir(path.join(__dirname, 'files'), (err, files) => {
-      if (err) throw err;
-      files.forEach((file) => {
-        fs.copyFile(
-          path.join(__dirname, 'files', file),
-          path.join(pathName, file),
-          (err) => {
-            if (err) throw err;
-          },
-        );
+  fs.rm(
+    path.join(__dirname, 'files-copy/'),
+    {
+      recursive: true,
+      force: true,
+    },
+    (err) => {
+      if (err) console.log(err);
+      fs.mkdir(pathName, { recursive: true }, (err) => {
+        if (err) console.log(err);
+        fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+          if (err) throw err;
+          files.forEach((file) => {
+            fs.copyFile(
+              path.join(__dirname, 'files', file),
+              path.join(pathName, file),
+              (err) => {
+                if (err) throw err;
+              },
+            );
+          });
+        });
       });
-    });
-  });
+    },
+  );
 }
 
 copyDir();
